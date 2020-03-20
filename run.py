5909f01c2e03d13f3e1f02599b14c9888b00aa60
GarySimons <garycsimons@gmail.com>
2020-03-20 06:33:00 +0000

    Added timestamp to messages

--- run.py
+++ run.py
@@ -1,31 +1,36 @@
 import os
+from datetime import datetime
 from flask import Flask, redirect
 
 app = Flask(__name__)
 messages = []
 
 def add_messages(username, message):
-    """Add messages to the messages list"""
-    messages.append("{}: {}".format(username, message))
+    """Add messages to the `messages` list"""
+    now = datetime.now().strftime("%H:%M:%S")
+    messages.append("({}) {}: {}".format(now, username, message))
 
 def get_all_messages():
-    """Get all messages and separate them with a 'br'"""
+    """Get all of the messages and separate them with a `br`"""
     return "<br>".join(messages)
 
-@app.route('/')
+@app.route("/")
 def index():
     """Main page with instructions"""
-    return "To send a message use /USERNAME/MESSAGE"
+    return "To send a message use: /USERNAME/MESSAGE"
 
-@app.route('/<username>')
+
+@app.route("/<username>")
 def user(username):
-    """Display chat message"""
+    """Display chat messages"""
     return "<h1>Welcome, {0}</h1>{1}".format(username, get_all_messages())
 
-@app.route('/<username>/<message>')
+
+@app.route("/<username>/<message>")
 def send_message(username, message):
     """Create a new message and redirect back to the chat page"""
     add_messages(username, message)
     return redirect("/" + username)
+
 
 app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)), debug=True)
